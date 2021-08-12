9e3dc5ead00985f2574d34cb1ab46868f607170d
Chester Koh <chester8991@gmail.com>
2021-08-13 01:07:27 +0800

    add missing texture warning

--- src/main/java/org/lwjglb/game/Main.java
+++ src/main/java/org/lwjglb/game/Main.java
@@ -2,11 +2,20 @@ package org.lwjglb.game;
 
 import org.lwjglb.engine.GameEngine;
 import org.lwjglb.engine.IGameLogic;
- 
+
+import java.io.File;
+
 public class Main {
  
     public static void main(String[] args) {
         try {
+            // Extract textures
+            File texturesFolder = new File("textures");
+            if(!texturesFolder.exists()){
+                System.err.println("Folder named textures not found! Please extract texture files in a folder named textures in the working directory first.");
+                System.exit(-1);
+                return;
+            }
             boolean vSync = true;
             IGameLogic gameLogic = new DummyGame();
             GameEngine gameEng = new GameEngine("Noodle Simulator 2021", 1024, 768, vSync, gameLogic);
